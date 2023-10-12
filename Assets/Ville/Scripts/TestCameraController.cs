3b04d468dc58fbc93c7f641bf5b93eb90814e6f7
VilleK88 <ville.karppinen88@gmail.com>
2023-10-12 09:35:37 +0300

    3D initialization

---
+++ Assets/Ville/Scripts/TestCameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraController : MonoBehaviour
+{
+    Camera cam;
+    [SerializeField] Transform player;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x, transform.position.y, player.position.z);
+    }
+}
---
+++ Assets/Ville/Scripts/TestPlayer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] GameObject childSprite;
+    public Camera cam;
+
+    [Header("Player Movement and Mouse Aiming")]
+    float moveSpeed = 5;
+    Vector3 movement;
+    Vector3 mousePos;
+    Vector3 lookDir;
+    float angle;
+
+    Ray ray;
+    RaycastHit hit;
+    Vector3 cursorPosition;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        childSprite.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Move();
+        MouseAiming();
+        Attack();
+    }
+
+    private void FixedUpdate()
+    {
+        FixedMove();
+    }
+
+    void Move()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedMove()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void MouseAiming()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            cursorPosition = hit.point - transform.position;
+
+            transform.forward = new Vector3(cursorPosition.x, 0, cursorPosition.z);
+        }
+    }
+
+    void Attack()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            childSprite.GetComponent<Animator>().SetTrigger("AxeAttack1");
+        }
+    }
+}
