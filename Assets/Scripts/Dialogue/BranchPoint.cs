852a81bc814e6ad8e43224deccddb6e984eda997
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2023-11-09 10:28:20 +0200

    Dialogue system in progress...

---
+++ Assets/Scripts/Dialogue/BranchPoint.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BranchPoint
+{
+    [TextArea]
+    public string question;
+    public Answer[] answers;
+}
