3e0e858903fc45fffccaad9b9c95421f8e709fba
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2023-11-17 13:01:44 +0200

    Quest reward system in progress...

--- Assets/Scripts/Quest/RewardManager.cs
+++ Assets/Scripts/Quest/RewardManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RewardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI questName;
+    public Image[] questRewardIcons;
+    public TextMeshProUGUI questRewardXp;
+    public GameObject questRewardUI;
 
-    // Update is called once per frame
-    void Update()
+    public void SetRewardUI(QuestBase quest)
     {
-        
+        questRewardUI.SetActive(true);
+
+        questName.text = quest.questName;
+
+        for(int i = 0; i < quest.rewards.itemRewards.Length; i++)
+        {
+            questRewardIcons[i].gameObject.SetActive(true);
+            questRewardIcons[i].sprite = quest.rewards.itemRewards[i].icon;
+        }
     }
 }
