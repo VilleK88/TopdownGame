3b680dda1d5b3d5902e8714071c31be24c78a0da
VilleK88 <126750132+VilleK88@users.noreply.github.com>
2023-12-19 13:20:48 +0200

    Settings menu in progress...

--- Assets/Scripts/UIScripts/Menu/Settings.cs
+++ Assets/Scripts/UIScripts/Menu/Settings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -8,6 +9,9 @@ public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
     [SerializeField] Slider volumeSlider;
+
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
 
     private void Start()
     {
@@ -20,6 +24,28 @@ public class Settings : MonoBehaviour
         {
             Load();
         }
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
     public void SetVolume(float volume)
