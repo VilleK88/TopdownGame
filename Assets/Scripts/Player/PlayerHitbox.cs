3648910aa56aedc1c89d85573073eef789a8adee
VilleK88 <ville.karppinen88@gmail.com>
2024-01-04 13:28:50 +0200

    Fixing questlog in progress...

--- Assets/Scripts/Player/PlayerHitbox.cs
+++ Assets/Scripts/Player/PlayerHitbox.cs
@@ -89,7 +89,7 @@ public class PlayerHitbox : MonoBehaviour
                     }
                     else
                     {
-                        health.TakeDamage(10 + damage.GetValue());
+                        health.TakeDamage(100 + damage.GetValue());
                     }
                 }
 
