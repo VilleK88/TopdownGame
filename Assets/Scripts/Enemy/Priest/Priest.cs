ab491b223df2519c097242d626e80702f1166b06
VilleK88 <ville.karppinen88@gmail.com>
2024-04-29 07:46:19 +0300

    Code optimization for proof of degree

--- Assets/Scripts/Enemy/Priest/Priest.cs
+++ Assets/Scripts/Enemy/Priest/Priest.cs
@@ -1,19 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class Priest : MonoBehaviour
 {
     [Header("Rigidbody, Colliders and Sprites")]
     Rigidbody rb;
     CapsuleCollider capsuleCollider;
     [SerializeField] GameObject childSprite;
-
     [Header("Death Parameters")]
     bool dead = false;
     bool deadFetch; // from TestPriestHealth -script
-
     [Header("Converting Peasants Parameters")]
     public Peasant[] peasants;
     Peasant peasant;
@@ -22,13 +18,6 @@ public class Priest : MonoBehaviour
     int currentPeasantIndex = 0;
     public bool convertedFetch;
     bool converting = false;
-
-    [Header("Particle Parameters")]
-    [SerializeField] GameObject particleConverting;
-    ParticleSystem particleSystemConverting;
-    [SerializeField] GameObject particleHealing;
-    ParticleSystem particleSystemHealing;
-
     [Header("Healing Enemies Parameters")]
     public bool startHealing = false;
     float checkInterval = 2;
@@ -38,7 +27,6 @@ public class Priest : MonoBehaviour
     GameObject enemy;
     EnemyHealth enemyHealth;
     GameObject previousEnemy;
-
     [Header("Field of View Parameters")]
     public float radius = 10;
     [Range(0, 360)]
@@ -50,31 +38,21 @@ public class Priest : MonoBehaviour
     Transform target;
     Vector3 directionToTarget;
     float distanceToTarget;
-
     [Header("Player")]
     GameObject player;
     Transform playerTransform;
-
     [Header("Chase, Attack and Agro Parameters")]
     public bool isAgro = false;
     float maxAgroCounter = 5;
     public float agroCounter = 0;
-    //Vector3 direction;
-    //Quaternion lookRotation;
     NavMeshAgent agent;
     bool healing = false;
     float distanceToPlayer;
     public bool activated;
-
     [Header("Hurt")]
-    //bool gettingHit;
-    //float hitMaxTime = 0.25f;
     public float hitCounter = 0;
-
     [Header("Audio")]
     AudioSource convertingSound;
-
-
     private void Start()
     {
         player = PlayerManager.instance.player;
@@ -82,23 +60,17 @@ public class Priest : MonoBehaviour
         agent = GetComponent<NavMeshAgent>();
         convertingSound = GetComponent<AudioSource>();
         peasants = FindObjectsOfType<Peasant>();
-        particleSystemConverting = particleConverting.GetComponent<ParticleSystem>();
-        particleSystemHealing = particleHealing.GetComponent<ParticleSystem>();
         capsuleCollider = GetComponent<CapsuleCollider>();
         InvokeRepeating("HealEnemies", 0, checkInterval);
         StartCoroutine(FOVRoutine());
     }
-
     private void Update()
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         deadFetch = GetComponent<EnemyHealth>().dead;
         Death();
         if(distanceToPlayer < 30)
-        {
             activated = true;
-        }
-
         if (!dead && activated)
         {
             if (canSeePlayer)
@@ -106,11 +78,8 @@ public class Priest : MonoBehaviour
                 isAgro = true;
                 agroCounter = 0;
                 StartCoroutine(CallHelp());
-
                 if (!healing && !converting)
-                {
                     transform.LookAt(player.transform.position);
-                }
             }
             else
             {
@@ -118,9 +87,7 @@ public class Priest : MonoBehaviour
                 {
 
                     if (agroCounter < maxAgroCounter)
-                    {
                         agroCounter += Time.deltaTime;
-                    }
                     else
                     {
                         agroCounter = 0;
@@ -128,26 +95,17 @@ public class Priest : MonoBehaviour
                     }
                 }
             }
-
             if (agent.remainingDistance <= agent.stoppingDistance && !deadFetch)
             {
                 if (!startHealing)
-                {
                     Convert();
-                }
                 else
-                {
                     agent.SetDestination(enemyHealth.transform.position);
-                }
             }
-
             if (deadFetch)
-            {
                 childSprite.GetComponent<Animator>().SetBool("Converting", false);
-            }
         }
     }
-
     IEnumerator FOVRoutine()
     {
         WaitForSeconds wait = new WaitForSeconds(0.2f);
@@ -158,60 +116,44 @@ public class Priest : MonoBehaviour
             FieldOfViewCheck();
         }
     }
-
     void FieldOfViewCheck()
     {
         rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
-
         if (rangeChecks.Length != 0)
         {
             target = rangeChecks[0].transform;
             directionToTarget = (target.position - transform.position).normalized;
-
             if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
             {
                 distanceToTarget = Vector3.Distance(transform.position, target.position);
 
                 if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
-                {
                     canSeePlayer = true;
-                }
                 else
-                {
                     canSeePlayer = false;
-                }
             }
             else
-            {
                 canSeePlayer = false;
-            }
         }
         else if (canSeePlayer)
-        {
             canSeePlayer = false;
-        }
     }
-
     void HealEnemies()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, detectionRadius);
-
         foreach(Collider collider in colliders)
         {
             if(collider.CompareTag(enemyTag))
             {
                 enemy = collider.gameObject;
                 float distance = Vector3.Distance(transform.position, enemy.transform.position);
-
                 if(distance <= detectionRadius)
                 {
                     NavMeshPath path = new NavMeshPath();
                     agent.CalculatePath(enemy.transform.position, path);
-
                     if(path.status == NavMeshPathStatus.PathComplete)
                     {
                         enemyHealth = enemy.GetComponent<EnemyHealth>();
-
                         if (enemyHealth != null && enemyHealth.currentHealth < maxHealth &&
                             enemyHealth.currentHealth > 0)
                         {
@@ -229,7 +171,6 @@ public class Priest : MonoBehaviour
             }
         }
     }
-
     void Convert()
     {
         peasant = GetCurrentPeasantScript();
@@ -268,20 +209,15 @@ public class Priest : MonoBehaviour
             converting = false;
         }
     }
-
     Peasant GetCurrentPeasantScript()
     {
         if (currentPeasantIndex >= 0 && currentPeasantIndex < peasants.Length)
-        {
             return peasants[currentPeasantIndex].GetComponent<Peasant>();
-        }
         return null;
     }
-
     IEnumerator CallHelp()
     {
         yield return new WaitForSeconds(0.5f);
-
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject enemy in enemies)
         {
@@ -290,66 +226,51 @@ public class Priest : MonoBehaviour
             Peasant peasant = enemy.GetComponent<Peasant>();
             Longbowman longbowman = enemy.GetComponent<Longbowman>();
             Priest priest = enemy.GetComponent<Priest>();
-
             if (enemyTransform != null && knight != null)
             {
                 float distance = Vector3.Distance(transform.position, enemyTransform.position);
                 if (distance < 20)
-                {
                     knight.isAgro = true;
-                }
             }
             if (enemyTransform != null && peasant != null)
             {
                 float distance = Vector3.Distance(transform.position, enemyTransform.position);
                 if (distance < 20)
-                {
                     peasant.isAgro = true;
-                }
             }
             if (enemyTransform != null && longbowman != null)
             {
                 float distance = Vector3.Distance(transform.position, enemyTransform.position);
                 if (distance < 20)
-                {
                     longbowman.isAgro = true;
-                }
             }
             if (enemyTransform != null && priest != null)
             {
                 float distance = Vector3.Distance(transform.position, enemyTransform.position);
                 if (distance < 20)
-                {
                     priest.isAgro = true;
-                }
             }
         }
     }
-
     void Death()
     {
         if (deadFetch)
         {
             dead = true;
             convertingSound.Stop();
-            //childSprite.GetComponent<Animator>();
             if (capsuleCollider != null)
-            {
                 capsuleCollider.enabled = false;
-            }
             this.enabled = false;
             gameObject.GetComponent<NavMeshAgent>().isStopped = true;
             CancelInvoke("HealEnemies");
             StartCoroutine(Vanish());
         }
     }
-
     IEnumerator Vanish()
     {
         yield return new WaitForSeconds(2);
         childSprite.GetComponent<SpriteRenderer>().enabled = false;
         childSprite.GetComponentInChildren<SpriteRenderer>().enabled = false;
-        //yield return new WaitForSeconds(2);
         gameObject.SetActive(false);
     }
 }
